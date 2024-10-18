9d3ece39c85d9f5147555a4b3f1baefc0f1e4a28
longbui2k3 <buiduclongit@gmail.com>
2024-10-18 20:35:00 +0700

    Fix check has company

--- frontend/src/pages/Cover.tsx
+++ frontend/src/pages/Cover.tsx
@@ -1,18 +1,7 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Footer, Header } from "../components/global";
-import { useAuthContext } from "../context";
-import { CREATE_COMPANY_KEY, getRoute } from "../helpers/constants";
 
 export default function Cover() {
-  const { user } = useAuthContext();
-  if (
-    typeof user !== "string" &&
-    user &&
-    user.hasCompany !== undefined &&
-    user.hasCompany === false
-  ) {
-    return <Navigate to={getRoute(CREATE_COMPANY_KEY).path} replace={true} />;
-  }
   return (
     <div className="w-full">
       <Header />
--- frontend/src/pages/PrivateRoutes.tsx
+++ frontend/src/pages/PrivateRoutes.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthContext } from "../context";
 import {
+  CREATE_COMPANY_KEY,
   getRoute,
   SIGN_IN_KEY,
 } from "../helpers/constants";
@@ -10,6 +11,9 @@ export default function PrivateRoutes() {
   if (typeof user === "string") return <></>;
   if (user === null) {
     return <Navigate to={getRoute(SIGN_IN_KEY).path} />;
+  }
+  if (user.hasCompany !== undefined && user.hasCompany === false) {
+    return <Navigate to={getRoute(CREATE_COMPANY_KEY).path} replace={true} />;
   }
   return <Outlet />;
 }
